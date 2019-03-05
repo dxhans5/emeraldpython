fce3b66c56518f4c0f1718f5aecd750ec220387d
donaldhansen <dxhans5@gmail.com>
2019-03-05 14:32:43 -0700

    trying to get images to d&d&sort

--- resources/js/components/ImageGalleryComponent.vue
+++ resources/js/components/ImageGalleryComponent.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Images</h5>
+  <div class="rootEl">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Images</h5>
 
-      <div class="row">
-        <div class="col-sm-2 mb-2" v-for="image in JSON.parse(this.images)">
-          <img :src="'/gallery-images/' + image" class="img-thumbnail">
+        <div class="row">
+          <SlickList lockAxis="y" v-model="sortedImages">
+            <SlickItem v-for="image in JSON.parse(this.images)" :index="index" :key="index">
+              <img
+                v-on:click="showImage"
+                :src="'/gallery-images/' + image"
+                class="img-thumbnail linkable"
+              >
+            </SlickItem>
+          </SlickList>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="imgModal" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img v-if="this.fullSizeImg" :src="this.fullSizeImg">
+          </div>
         </div>
       </div>
     </div>
@@ -13,8 +37,25 @@
 </template>
 
 <script>
+import { ContainerMixin, ElementMixin } from "vue-slicksort";
+
 export default {
   props: ["images"],
+  data: function() {
+    return {
+      fullSizeImg: null
+    };
+  },
+  components: {
+    SlickItem,
+    SlickList
+  },
+  methods: {
+    showImage: function(e) {
+      this.fullSizeImg = e.currentTarget.getAttribute("src");
+      $("#imgModal").modal("show");
+    }
+  },
   mounted() {}
 };
 </script>
