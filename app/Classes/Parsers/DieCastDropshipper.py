c1091774e53ea41319a7fc45cfdaac582eeef602
donaldhansen <donaldhansen@clearwateranalytics.com>
2019-03-22 13:18:21 -0600

    more work on parsers

--- app/Classes/Parsers/DieCastDropshipper.py
+++ app/Classes/Parsers/DieCastDropshipper.py
@@ -51,8 +51,21 @@ def parseData(obj, param, rule):
     elif 'object' in rule:
         obj[param] = rule['object']
     elif 'content' in rule:
+        obj[param] = rule['content'][0]['content']
 
     return obj
+
+
+def download_image(image_url, path):
+    session = requests.Session()
+    local_filename = image_url.split('/')[-1].split("?")[0]
+
+    r = session.get(image_url, stream=True, verify=False)
+    with open(path + local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            f.write(chunk)
+
+    return local_filename
 
 
 # launch url
@@ -68,6 +81,49 @@ SOUP = BeautifulSoup(DRIVER.page_source, 'lxml')
 title = SOUP.select("h1[itemprop='name']")
 brand = SOUP.select(".BrandName > a > span")
 price = SOUP.select("meta[itemprop='price']")
+productId = str(uuid.uuid4())
+
+bullets = []
+for item in SOUP.select('.ProductDescriptionContainer > ul > li'):
+    bullets.append(item.text)
+bullets = list(dict.fromkeys(bullets))  # Remove any duplicates
+
+# Diecast Models uses a standard thumbnail > large image modal
+# Create the image directory
+path = '/vagrant/public/gallery-images/' + productId + '/'
+try:
+    os.mkdir(path)
+except OSError:
+    sys.exit("creation of the directory %s failed" % path)
+
+
+DRIVER.find_element_by_xpath(
+    "//*[@id='ProductDetails']/div/div[1]/div/div[2]/a/img").click()
+overlay = WebDriverWait(DRIVER, 60).until(
+    EC.presence_of_element_located((By.ID, "fancy_frame")))
+SOUP2 = BeautifulSoup(DRIVER.page_source, 'lxml')
+iframe = SOUP2.select('#fancy_frame')
+
+# Get iframe content
+attemptConnection(iframe[0]['src'])
+SOUP3 = BeautifulSoup(DRIVER.page_source, 'lxml')
+print(SOUP3)
+
+imgs = []
+
+for overlayThumb in SOUP2.findAll(".TinyOuterDiv"):
+    # Get the individual xpath for each of the valid thumbnails
+    xpath = SoupXPath.xpath_soup(overlayThumb)
+    # Click on the individual thumbnail
+    DRIVER.find_element_by_xpath(xpath).click()
+    # Rescan the source to get the updated main image
+    SOUP3 = BeautifulSoup(DRIVER.page_source, 'lxml')
+
+    img_element = SOUP3.select(".ProductZoomImage > img")
+    img = img_element[0]['src']
+    fileName = download_image(img, path)
+
+    imgs.append(productId + '/' + fileName)
 
 # We're done scraping
 DRIVER.quit()
@@ -77,13 +133,13 @@ data = {}
 data = parseData(data, 'title', {'text': title})
 data = parseData(data, 'brand', {'text': brand})
 data = parseData(data, 'price', {'content': price})
-# data = parseData(data, 'bullets', {'object': bullets})
-# data = parseData(data, 'images', {'object': imgs})
+data = parseData(data, 'bullets', {'object': bullets})
+data = parseData(data, 'images', {'object': imgs})
 # data = parseData(data, 'dimensions', {'object': dimensions})
 # data = parseData(data, 'details', {'object': details})
 # data = parseData(data, 'sku', {'text': sku})
 # data = parseData(data, 'model', {'text': model})
 # data = parseData(data, 'description', {'text': description})
-# data = parseData(data, 'productId', {'object': productId})
+data = parseData(data, 'productId', {'object': productId})
 
-print(json.dumps(data))
+# print(json.dumps(data))
